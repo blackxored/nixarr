b62ceb975e855683400037a928ef7d93ef239d67
Adrian Perez <133308+blackxored@users.noreply.github.com>
2025-11-13 17:31:42 -0800

    more declarative sabnzbd

--- nixarr/sabnzbd/default.nix
+++ nixarr/sabnzbd/default.nix
@@ -129,6 +129,64 @@ in {
         > Is not supported, because `/home/user` is owned by `user`.
       '';
     };
+
+    manageDirs = mkOption {
+      type = types.bool;
+      default = true;
+      description = ''
+        Automatically create and manage required dirs under media.
+        If set to false only ${cfg.stateDir} would be managed.
+      '';
+    };
+
+    manageConfig = mkOption {
+      type = types.bool;
+      default = cfg.configFile == null;
+      internal = true;
+      visible = false;
+    };
+
+    configFile = mkOption {
+      type = types.nullOr types.path;
+      default = null;
+      description = ''
+        Config file, if you wish to override the config entirely
+      '';
+    };
+
+    downloadDir = mkOption {
+      type = types.path;
+      default = cfg.incompleteDir;
+    };
+
+    completeDir = mkOption {
+      type = types.path;
+      default = "${nixarr.mediaDir}/usenet/manual";
+    };
+
+    watchDir = mkOption {
+      type = types.path;
+      default = "${nixarr.mediaDir}/usenet/.watch";
+    };
+
+    secretFile = mkOption {
+      type = types.nullOr types.path;
+      default = null;
+    };
+
+    settings = mkOption {
+      type = types.attrs;
+      default = {};
+    };
+
+    categories = mkOption {
+      type = types.attrsOf (types.submodule {
+        options.dir = lib.mkOption {
+          type = lib.types.str;
+        };
+      });
+      default = {};
+    };
   };
 
   config = let
@@ -149,14 +207,54 @@ in {
           then "192.168.15.1"
           else "127.0.0.1";
         port = cfg.guiPort;
-        download_dir = "${cfg.incompleteDir}/usenet/.incomplete";
-        complete_dir = "${nixarr.mediaDir}/usenet/manual";
-        dirscan_dir = "${nixarr.mediaDir}/usenet/watch";
+        download_dir = cfg.downloadDir;
+        complete_dir = cfg.completeDir;
+        dirscan_dir = cfg.watchDir;
         host_whitelist = concatStringsCommaIfExists cfg.whitelistHostnames;
         local_ranges = concatStringsCommaIfExists cfg.whitelistRanges;
         permissions = "775";
       };
     };
+
+    categories =
+      [
+        {
+          name = "*";
+          order = 0;
+          pp = 3;
+          script = "None";
+          dir = "";
+          newzbin = "";
+          priority = 0;
+        }
+      ]
+      ++ lib.imap1 (i: v: {
+        order = i;
+        name = v.name;
+        pp = "";
+        script = "Default";
+        dir = v.dir;
+        newzbin = "";
+        priority = -100;
+      }) (lib.mapAttrsToList (n: v: {name = n;} // v) cfg.categories);
+
+    mkCategoryPython = cat: ''
+      sab_config_map['categories']['${cat.name}'] = {
+        'name': '${cat.name}',
+        'order': ${toString cat.order},
+        'pp': ${
+        if cat.pp == ""
+        then "''"
+        else toString cat.pp
+      },
+        'script': '${cat.script}',
+        'dir': '${cat.dir}',
+        'newzbin': '${cat.newzbin}',
+        'priority': ${toString cat.priority}
+      }
+    '';
+
+    categoriesPython = lib.concatStringsSep "\n" (map mkCategoryPython categories);
 
     ini-base-config-file = pkgs.writeTextFile {
       name = "base-config.ini";
@@ -206,6 +304,19 @@ in {
 
         ${lib.strings.concatStringsSep "\n" user-configs-to-python-list}
 
+        sab_config_map['categories'] = {};
+        ${categoriesPython}
+
+        ${lib.optionalString (cfg.secretFile != null)
+          #python
+          ''
+            secrets_path = Path("${cfg.secretFile}")
+            if not secrets_path.is_file():
+                raise Exception(f"{secrets_path} is not a valid config file path.")
+
+            sab_config_map.merge(ConfigObj(str(secrets_path)))
+          ''}
+
         sab_config_map.write()
       '';
   in
@@ -236,36 +347,44 @@ in {
         };
       };
 
-      systemd.tmpfiles.rules = [
-        "d '${cfg.stateDir}' 0700 ${globals.sabnzbd.user} root - -"
-        "C ${cfg.stateDir}/sabnzbd.ini - - - - ${ini-base-config-file}"
+      systemd.tmpfiles.rules =
+        [
+          "d '${cfg.stateDir}' 0700 ${globals.sabnzbd.user} root - -"
+          "C ${cfg.stateDir}/sabnzbd.ini - - - - ${ini-base-config-file}"
 
-        # Media dirs
-        "d '${nixarr.mediaDir}/usenet'               0755 ${globals.sabnzbd.user} ${globals.sabnzbd.group} - -"
-        "d '${cfg.incompleteDir}/usenet/.incomplete' 0755 ${globals.sabnzbd.user} ${globals.sabnzbd.group} - -"
-        "d '${nixarr.mediaDir}/usenet/.watch'        0755 ${globals.sabnzbd.user} ${globals.sabnzbd.group} - -"
-        "d '${nixarr.mediaDir}/usenet/manual'        0775 ${globals.sabnzbd.user} ${globals.sabnzbd.group} - -"
-        "d '${nixarr.mediaDir}/usenet/lidarr'        0775 ${globals.sabnzbd.user} ${globals.sabnzbd.group} - -"
-        "d '${nixarr.mediaDir}/usenet/radarr'        0775 ${globals.sabnzbd.user} ${globals.sabnzbd.group} - -"
-        "d '${nixarr.mediaDir}/usenet/sonarr'        0775 ${globals.sabnzbd.user} ${globals.sabnzbd.group} - -"
-        "d '${nixarr.mediaDir}/usenet/shelfmark'     0775 ${globals.sabnzbd.user} ${globals.sabnzbd.group} - -"
-      ];
+          "d '${nixarr.mediaDir}/usenet'             0755 ${globals.sabnzbd.user} ${globals.sabnzbd.group} - -"
+          "d '${cfg.completeDir}'                    0755 ${globals.sabnzbd.user} ${globals.sabnzbd.group} - -"
+          "d '${cfg.watchDir}'                       0755 ${globals.sabnzbd.user} ${globals.sabnzbd.group} - -"
+          "d '${cfg.downloadDir}'                    0755 ${globals.sabnzbd.user} ${globals.sabnzbd.group} - -"
+        ]
+        ++ lib.optional cfg.manageConfig "C ${cfg.stateDir}/sabnzbd.ini - - - - ${ini-base-config-file}"
+        ++ lib.optionals cfg.manageDirs [
+          # Media dirs
+          "d '${nixarr.mediaDir}/usenet/manual'        0775 ${globals.sabnzbd.user} ${globals.sabnzbd.group} - -"
+          "d '${nixarr.mediaDir}/usenet/lidarr'      0775 ${globals.sabnzbd.user} ${globals.sabnzbd.group} - -"
+          "d '${nixarr.mediaDir}/usenet/radarr'      0775 ${globals.sabnzbd.user} ${globals.sabnzbd.group} - -"
+          "d '${nixarr.mediaDir}/usenet/sonarr'      0775 ${globals.sabnzbd.user} ${globals.sabnzbd.group} - -"
+          "d '${nixarr.mediaDir}/usenet/shelfmark'     0775 ${globals.sabnzbd.user} ${globals.sabnzbd.group} - -"
+        ];
 
       services.sabnzbd = {
         enable = true;
         package = cfg.package;
         user = globals.sabnzbd.user;
         group = globals.sabnzbd.group;
-        configFile = "${cfg.stateDir}/sabnzbd.ini";
+        configFile =
+          if (cfg.configFile != null)
+          then cfg.configFile
+          else "${cfg.stateDir}/sabnzbd.ini";
       };
 
       networking.firewall.allowedTCPPorts = mkIf cfg.openFirewall [cfg.guiPort];
 
       systemd.services.sabnzbd.serviceConfig = {
-        ExecStartPre = lib.mkBefore [
+        ExecStartPre = lib.mkIf cfg.manageConfig (lib.mkBefore [
           ("+" + fix-config-permissions-script + "/bin/sabnzbd-fix-config-permissions")
           (apply-user-configs-script + "/bin/sabnzbd-set-user-values")
-        ];
+        ]);
         Restart = "on-failure";
         StartLimitBurst = 5;
       };
